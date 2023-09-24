553e7e8bf5ca998520245eeaa34dfa9b05ce378a
Lydia Hsieh <hch34242@gmail.com>
2023-09-24 19:39:01 +0800

    hw3 Question03 duplicate numbers fixed

--- src/hw3/Question03.java
+++ src/hw3/Question03.java
@@ -2,6 +2,8 @@ package hw3;
 
 import java.util.Scanner;
 
+import java.util.Arrays;
+
 public class Question03 {
 
 	public static void main(String[] args) {
@@ -33,21 +35,29 @@ public class Question03 {
 		
 		
 		// 亂數印出6個號碼且不重複
-		// 重複未解決
+		// 重複已解決
 		int count;
+		int[] numbers = new int[6]; // 用來放已隨機出的數字，以便使用Arrays.binarySearch()方法檢查
+		
 		for (count = 0; count < 6; count++) {
 			int j = (int)(Math.random()*49)+1;
-			if((j % 10 == i || (j >= i * 10 && j <= i * 10 + 9))) {
+			if( j % 10 == i || ((j >= i * 10 && j <= i * 10 + 9))) {
 				count--;
 				continue;
-			}
-			else {
-				System.out.print(j+" ");
+			}else if(Arrays.binarySearch(numbers, j) >= 0) {
+				count--;
+				continue;
+			}else {
+				numbers[0] = j;  // 因為下一行會執行排序，所以index不用numbers，用排序後一定會空下來的(有初始值0的)index 0位置
+				Arrays.sort(numbers);
 			}
 		}
-				
-		
 
+		for(int k = 0; k < numbers.length; k++) {
+			System.out.print(numbers[k] + " ");
+		}
+		
+		
 		s.close();
 	}
 }
